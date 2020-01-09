541f376689fa3c2f7825cdc20ef24d6cc79b1721
fpaschos <fpaschos@gmail.com>
2020-01-09 03:02:01 +0200

    Fixed diagram responsiveness.

--- react-ui/src/components/AccountPlot.js
+++ react-ui/src/components/AccountPlot.js
@@ -13,28 +13,43 @@ import {
 
 import 'react-vis/dist/style.css';
 
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {useWindowSize} from "../services/hooks";
 
 const AccountPlot = (props) => {
 
-    const d = props.data;
+    const {data} = props;
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const ref = useRef(null);
+
+    const size = useWindowSize();
+
+    // responsive width and height
+    useEffect(() => {
+        setWidth(ref.current.clientWidth);
+        // setHeight(ref.current.clientHeight > 400 ? ref.current.clientHeight : 400);
+        setHeight(400)
+    }, [size]);
+
 
     return (
-        // <p>{JSON.stringify(data)}</p>
-        <XYPlot
-            width={300}
-            height={300}
-            xType="time"
-            // style={{ fill: 'none'}}
+        <div style={{width: '100%', height: '100%'}} ref={ref}>
+            <XYPlot
+                width={width}
+                height={height}
+                xType="time"
+                // style={{ fill: 'none'}}
 
-        >
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <XAxis />
-            <YAxis />
-            <LineSeries
-                data={d}/>
-        </XYPlot>
+            >
+                <VerticalGridLines/>
+                <HorizontalGridLines/>
+                <XAxis/>
+                <YAxis/>
+                <LineSeries
+                    data={data}/>
+            </XYPlot>
+        </div>
     );
 };
 
--- react-ui/src/services/hooks.ts
+++ react-ui/src/services/hooks.ts
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 // From https://usehooks.com/usePrevious/
 export let usePrevious: (value: any) => any;
@@ -17,3 +17,32 @@ usePrevious = (value: any) => {
     return ref.current;
 };
 
+
+// From https://usehooks.com/useWindowSize/
+export const  useWindowSize = () => {
+    const isClient = typeof window === 'object';
+
+    function getSize() {
+        return {
+            width: isClient ? window.innerWidth : undefined,
+            height: isClient ? window.innerHeight : undefined
+        };
+    }
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+        if (!isClient) {
+            return () => {};
+        }
+
+        function handleResize() {
+            setWindowSize(getSize());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+
+    return windowSize;
+}
